c8aa61c4cda1a57e3e94e53dc0245c52843c58a3
triad01 <luckypee01@gmail.com>
2024-07-23 22:21:59 +0100

    task 1

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array) {
+  for (let idx of array) {
     const value = array[idx];
     array[idx] = appendString + value;
   }
