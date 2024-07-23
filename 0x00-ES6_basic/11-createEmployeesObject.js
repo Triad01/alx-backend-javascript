537d932f4ef5fac146654bf0a99249a7abac2bc2
triad01 <luckypee01@gmail.com>
2024-07-23 22:50:57 +0100

    task 11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,7 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable */
 export default function createEmployeesObject(departmentName, employees) {
-  return { departmentName: employees };
-}
+  const departmentEmployeesObject = {
+    [departmentName]: employees,
+  };
 
+  return departmentEmployeesObject;
+}
