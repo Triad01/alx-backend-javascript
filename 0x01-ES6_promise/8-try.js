551054a5db554bff43b3eea3506a90c9e62e2b75
triad01 <luckypee01@gmail.com>
2024-07-24 21:49:32 +0100

    task 8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -2,17 +2,6 @@
 /* eslint-disable */
 
 export default function divideFunction(numerator, denominator) {
-    try{
-     
-        if (denominator === 0) throw new Error("cannot divide by 0")
-        else {
-            const result = numerator / denominator
-            return result
-        }
-    }
-    catch{
-        throw new Error("cannot divide by 0")
-        
-    }
-
-}
+    if (denominator === 0) throw new Error('cannot divide by 0');
+    return (numerator / denominator);
+  }
