40897c155ec2f9dbda1b60699d3ba6544a39ee36
triad01 <luckypee01@gmail.com>
2024-07-24 17:12:51 +0100

    task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,16 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
-export default  function getFullResponseFromAPI(success){
-    return new Promise((resolve, reject)=>{
-        if (success) resolve({status: "200", body:"Success"})
-        else reject("The fake API is not working currently")
-    })
+// 1-promise.js
+
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+        if (success) {
+            resolve({
+                status: 200,
+                body: 'Success'
+            });
+        } else {
+            reject(new Error('The fake API is not working currently'));
+        }
+    });
 }
