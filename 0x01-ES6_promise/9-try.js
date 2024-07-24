e1e5e58509a0f5a2453140cbe6eecf89ebadc2ba
triad01 <luckypee01@gmail.com>
2024-07-24 21:50:46 +0100

    task 8

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,3 +1,11 @@
-export default function guardrail(){
-
-}
+export default function guardrail(mathFunction) {
+    const queue = [];
+    try {
+      queue.push(mathFunction());
+    } catch (error) {
+      queue.push(error.message);
+    } finally {
+      queue.push('Guardrail was processed');
+    }
+    return queue;
+  }
