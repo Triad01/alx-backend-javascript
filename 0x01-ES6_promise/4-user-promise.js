9f2c33af350cafb1deec97af80c3f35683a209ea
triad01 <luckypee01@gmail.com>
2024-07-24 18:21:02 +0100

    task 4

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+
+export default function signUpUser(firstName, lastName) {
+    return Promise.resolve({
+        firstName,
+        lastName
+    })
+}
