8aff10ed149e994b5f29fa77406fff602029e283
triad01 <luckypee01@gmail.com>
2024-07-29 18:43:14 +0100

    fixed code requirement for task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,20 +1,27 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
-function cleanSet(set, startString) {
-    if (startString === '' || typeof startString !== 'string') {
-      return '';
-    }
-  
-    let result = [];
-  
-    for (const element of set) {
-      if (element.startsWith(startString)) {
-        result.push(element.slice(startString.length));
+/**
+ * Joins a set of strings with a dash after stripping the strings of
+ * a leading sub string.
+ * @param {Set<String>} set - A collection of strings.
+ * @param {String} startString - The string to strip from the beginning
+ * of each item in the set.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns {String}
+ */
+export default function cleanSet(set, startString) {
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
       }
     }
-  
-    return result.join('-'); /* separates element of the array into a string using a speciffied delimeter */
   }
-  
-  export default cleanSet;
-  
+  return parts.join('-');
+}
