46f19486faa27fe8b6f08f29485ea960d5c67e8a
triad01 <luckypee01@gmail.com>
2024-07-29 18:05:41 +0100

    fixed task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,23 +1,11 @@
 #!/usr/bin/node
-const getListStudents = ()=>{
-    return [
-        {
-            id: "1",
-            firstName: "Guillaume",
-            location: "San Francisco"
-        }, {
-            
-            id: "2",
-            firstName: "James",
-            location: "Columbia"
-        },
-        {
-            
-            id: "5",
-            firstName: "Serena",
-            location: "San Francisco"
-        }
-        
-    ]
-}
-export default getListStudents
+
+export default function getListStudents() {
+    const students = [
+      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+      { id: 2, firstName: 'James', location: 'Columbia' },
+      { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    ];
+  
+    return students;
+  }
