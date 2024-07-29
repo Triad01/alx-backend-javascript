867fbe02ff25ca86bb1ad98c2eb2486cd6a5ed7a
triad01 <luckypee01@gmail.com>
2024-07-29 18:29:07 +0100

    fixed code requirement for task 6

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,9 +1,7 @@
-/**
- * Creates a set from an array.
- * @param {Array} array - The source array.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- * @returns {Set}
- */
-export default function setFromArray(array) {
-    return new Set(array);
-  }
+#!/usr/bin/node
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+
+const setFromArray = (array)=>{
+    return new Set(array)
+}
