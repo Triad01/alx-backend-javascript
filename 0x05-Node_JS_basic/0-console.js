04d488ac521954af172c7a4f1d16a4fd4bb9a864
triad01 <luckypee01@gmail.com>
2024-09-18 02:58:18 +0100

    task 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,10 @@
-const displayMessage = function(d) {
-  process.stdout.write(d + '\n');
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Chigboo Goodluck <https://github.com/Triad01>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
 };
+
 module.exports = displayMessage;
